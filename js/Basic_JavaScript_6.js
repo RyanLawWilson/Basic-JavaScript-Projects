4d6220fd8f97fa26286fda4af82356be50aca0f4
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-01-09 10:31:25 -0700

    Add CSS
    I added a little bit of CSS to make the page look better

--- js/Basic_JavaScript_6.js
+++ js/Basic_JavaScript_6.js
@@ -4,3 +4,26 @@ function Ride_Function() {
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + " to ride";
 }
+
+function ageValidation() {
+    var age, vote;
+    age = document.getElementById("age").value;
+    vote = (age >= 18) ? "You can vote!":"You need to be at least 18 to vote";
+    document.getElementById("vote").innerHTML = vote;
+}
+
+function Vehicle(make, model, year, color) {
+    this.Vehicle_make = make;
+    this.Vehicle_model = model;
+    this.Vehicle_year = year;
+    this.Vehicle_color = color;
+}
+
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+
+function myFunction() {
+    document.getElementById("constructors").innerHTML =
+    "Erik drives a " + Erik.Vehicle_color + "-colored " + Erik.Vehicle_model + " manufactured in " + Erik.Vehicle_year;
+}
